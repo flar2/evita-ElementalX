1626db21b16fc6996de35b521d84aeee8498ff34
flar2 <asegaert@gmail.com>
2013-05-11 12:27:52 -0400

    various fixes

--- drivers/misc/pm8xxx-vibrator-pwm.c
+++ drivers/misc/pm8xxx-vibrator-pwm.c
@@ -161,11 +161,6 @@ retry:
 	}
 }
 
-int vibrate(int time) {
-	pm8xxx_vib_enable(&vib_dev->timed_dev, time);
-	return 0;
-}
-
 static void pm8xxx_vib_update(struct work_struct *work)
 {
 	struct pm8xxx_vib_pwm *vib = container_of(work, struct pm8xxx_vib_pwm,
